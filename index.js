503cb4cbcc98c8fe3018ff618c902c3b1a255ce4
Laahkay <lonerkiddow15@gmail.com>
2022-08-31 14:04:38 +0200

    adding blog about react router dom

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ addToGroceryList({name:'Pap', price:20 , quantity: 1})
 // Create a function that removes items based on index groceryList:
 function removeBasedOnIndex(index){
     // code in here
-
+    console.log('groceryList11111', groceryList)
 
    return groceryList.splice(index, 1)
 
 }
 console.log(removeBasedOnIndex(0))
-
+console.log('groceryList22222', groceryList)
 // output: groceryList must be [{name:'Pop', price:20 , quantity: 1}]
 
 
@@ -29,13 +29,13 @@ console.log(removeBasedOnIndex(0))
 const calculateTotal = () =>{
 // code in here
 groceryList.forEach(item => {
-    total += item.price 
+    total += item.price * item.quantity
 })
 return total
 }
 
 // // function must return 20
-console.log("total",calculateTotal())
+console.log(calculateTotal())
 
 
 // console.log(groceryList)
